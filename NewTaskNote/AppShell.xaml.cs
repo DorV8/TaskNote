72aeec5c2d76f2b07db71d868ea8f78ab5af0246
TEMP <TEMP@DESKTOP-7H3J4F7>
2024-04-21 17:09:31 +0300

    Добавьте файлы проекта.

---
+++ NewTaskNote/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NewTaskNote
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NewTaskNote/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NewTaskNote
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
